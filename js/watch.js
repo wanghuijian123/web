4efab9fe141a826693f214990b3f0f1898e45262
wanghuijian123 <2389318125@qq.com>
2019-12-15 22:30:24 +0800

    提交

---
+++ js/watch.js
@@ -0,0 +1,55 @@
+
+	var setInter = 0;
+	main();
+	function main(){ 
+	    setInter = setInterval(function() {
+	        var vid = document.getElementById("videos");
+	        var currentTime=vid.currentTime.toFixed(1);
+	        console.log("时间"+currentTime);
+	        if(currentTime > 5 ){
+	            chickMe();
+	        }
+	    }, 1500);
+	}
+	
+	function chickMe() {
+	    videos.pause();
+	   
+	    let d2=document.getElementById("d2");
+	    d2.style.display="block";
+	    let d1=document.getElementById("d1");
+	    d1.style.display="block";
+	}
+	function cancel() {
+	    videos.pause();
+	    let d2=document.getElementById("d2");
+	    d2.style.display="none";
+	    let d1=document.getElementById("d1");
+	    d1.style.display="none";
+	    clearInterval(setInter);
+	    console.log("您点击了取消");
+	}
+	function confirm() {
+	    console.log("您点击了确定");
+	    let d2=document.getElementById("d2");
+	    d2.style.display="none";
+	    let d1=document.getElementById("d1");
+	    d1.style.display="none";
+	    videos.play();
+	    clearInterval(setInter);
+	}
+	function closes(){
+	    videos.pause();
+	    let d2=document.getElementById("d2");
+	    d2.style.display="none";
+	    let d1=document.getElementById("d1");
+	    d1.style.display="none";
+	    console.log("点击关闭");
+	}
+	function stu_ques(){
+	    videos.pause();
+	    let d2=document.getElementById("d2");
+	    d2.style.display="block";
+	    let d1=document.getElementById("d1");
+	    d1.style.display="block";
+	}
